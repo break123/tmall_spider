477fd37b69719510dd583bd529d7e2d4abcec694
Michael2011 <ky150@126.com>
2013-09-27 21:59:50 +0800

    tmall_spider
    tmall_spider

---
+++ tmall_spider/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for tmall_spider project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'tmall_spider'
+
+SPIDER_MODULES = ['tmall_spider.spiders']
+NEWSPIDER_MODULE = 'tmall_spider.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'tmall_spider (+http://www.yourdomain.com)'
